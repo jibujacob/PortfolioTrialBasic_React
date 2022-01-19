3473ce934a2652cd0465d0e6c2a969e85f6dc6e6
jibujacob <jibupjacob@gmail.com>
2022-01-19 18:58:45 +0530

    Ecommerce Site Added

--- client/src/dummydata.js
+++ client/src/dummydata.js
@@ -2,32 +2,29 @@
   export const webPortfolio = [
     {
       id: 1,
-      title: "Frontend Mentor Challenges",
-      img: "/assets/frontendMentorChallenges.png",
-      link:"https://jpj-frontendmentor-solutions.herokuapp.com/"
-    },
-    {
-      id: 2,
-      title: "Blog Site using Microservices",
-      img: "/assets/blog-microservices.png",
-      link: "https://github.com/jibujacob/Blog_MERN"
-    },
-    {
-      id: 3,
       title: "Branding SPA",
       img: "/assets/Branding_SPA.png",
       link:"https://jpj-branding-template.herokuapp.com/"
     },
     {
-      id: 4,
-      title: "Web Relax App",
-      img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
+      id: 2,
+      title: "Blog using Microservices",
+      img: "/assets/blog-microservices.png",
+      link: "https://github.com/jibujacob/Blog_MERN"
     },
     {
-      id: 5,
-      title: "Web Design",
-      img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+      id: 3,
+      title: "Ecommerce App using Microservices",
+      img: "/assets/Ecommerce.png",
+      link: "https://github.com/jibujacob/E-commerce_MERN"
     },
+
+    {
+      id: 4,
+      title: "Frontend Mentor Challenges",
+      img: "/assets/frontendMentorChallenges.png",
+      link:"https://jpj-frontendmentor-solutions.herokuapp.com/"
+    }
 
   ];
   
